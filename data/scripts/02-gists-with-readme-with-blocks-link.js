d201ae449effd1e2ec5755a2285d3f4bc5e28d2d
micahstubbs <micah.stubbs@gmail.com>
2015-11-19 19:44:09 -0800

    now detecting bl.ocks.org links

--- data/scripts/02-gists-with-readme-with-blocks-link.js
+++ data/scripts/02-gists-with-readme-with-blocks-link.js
@@ -6,24 +6,41 @@ var _ = require('lodash');
 "use strict";
 
 var withReadme = JSON.parse(fs.readFileSync('../gist-metadata/gists-with-readme.json', 'utf-8'));
+//console.log("withReadme", withReadme);
 
 var withBlocksLinks = [];
 
-withReadme.forEach(function(d) {
+withReadme.some(function(d) {
 
 	// get the README.md for this gist
+	var readmePath = "../gists/" + d["gistId"] + "/README.md"
+
+	// some error handling 
+	// var gistPath = "../gists/" + d["gistID"] + "readme.md"
+
+	var readme = fs.readFileSync(readmePath, 'utf-8');
+
+	var re = /bl\.ocks\.org/;
+
+	if(re.test(readme)) {
+		console.log(readme);
+
+		return true;
+	}
+
 
 	// check for any bl.ocks.org links
 
 	// if found, set hasBlocksLink to true and push the links to the blocksLinked array
 
-	if(hasBlocksLink) {
+	if(typeof hasBlocksLink !== "undefined") {
 		d["blocksLinked"] = []; // an array of the bl.ocks.org links found in the README.md
 
 		console.log("d", d);
 
 		withBlocksLinks.push(d);
 	}
+
 })
 
 console.log(withReadme.length + " README.md files in the d3 gists corpus")
